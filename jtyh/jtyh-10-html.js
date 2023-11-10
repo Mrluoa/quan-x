0d4debe7e3c3211b0064d716599a2adae0f0b09d
mrluoa <luoy@cdsimayi.com.cn>
2023-11-10 10:39:52 +0800

    fix:update

--- jtyh/jtyh-10-html.js
+++ jtyh/jtyh-10-html.js
@@ -20,8 +20,6 @@ var flgReplace_str = 'var previewFlg = "1";'
 
 var body = $response.body;
 
-console.log("时间匹配：" + body.replace(timeRegex, timeReplace_str));
-
 body = body.replace(timeRegex, timeReplace_str).replace(timeEmpRegex, timeReplace_str).replace(flgRegex, flgReplace_str);
 
 $done({ body: body });
