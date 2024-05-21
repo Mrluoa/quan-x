d7425be708ef40edf88a473c3f2432a5d8e9e48e
mrluoa <luoy@cdsimayi.com.cn>
2024-05-21 18:50:58 +0800

    fix:update

---
+++ my/performance.js
@@ -0,0 +1,17 @@
+/**
+* my-预约
+
+**/
+
+var body = $response.body;
+var obj = JSON.parse(body);
+
+
+obj.data.ticketStatus = 2;
+obj.data.saleRemindVO.onSaleTime = Date.now();
+obj.data.saleRemindVO.onSaleStatus = 2;
+obj.data.saleRemindVO.needCountDown = true;
+obj.data.saleRemindVO.countdownTime = 1710991080000;
+
+body = JSON.stringify(obj);
+$done({ body });
