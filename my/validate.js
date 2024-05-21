0846496132346828a48379343714dbc25f18a62d
mrluoa <luoy@cdsimayi.com.cn>
2024-05-21 18:23:17 +0800

    fix:update

--- my/validate.js
+++ my/validate.js
@@ -7,7 +7,7 @@ var body = $response.body;
 var obj = JSON.parse('{"code":200,"msg":"","data":null,"paging":null,"attrMaps":{"serverTime":1716286607481},"success":true}');
 
 
-obj.data.attrMaps.serverTime = Date.now();
+obj.attrMaps.serverTime = Date.now();
 body = JSON.stringify(obj);
 
 $done({ body });
