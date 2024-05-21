0e53d6f606fae70f4aea8297304caa59940ce9bd
mrluoa <luoy@cdsimayi.com.cn>
2024-05-21 18:19:51 +0800

    fix:update

---
+++ my/validate.js
@@ -0,0 +1,13 @@
+/**
+* my-预约
+
+**/
+
+var body = $response.body;
+var obj = JSON.parse('{"code":200,"msg":"","data":null,"paging":null,"attrMaps":{"serverTime":1716286607481},"success":true}');
+
+
+obj.data.attrMaps.serverTime = Date.now();
+body = JSON.stringify(obj);
+
+$done({ body });
