a4167e5f0a975569616277120cfec1c3253450fe
mrluoa <luoy@cdsimayi.com.cn>
2024-05-21 19:01:43 +0800

    fix:update

--- my/performance.js
+++ my/performance.js
@@ -8,9 +8,8 @@ var obj = JSON.parse(body);
 
 
 obj.data.ticketStatus = 2;
-//obj.data.saleRemindVO.onSaleTime = Date.now();
+obj.data.saleRemindVO.onSaleTime = Date.now();
 obj.data.saleRemindVO.onSaleStatus = 2;
-obj.data.saleRemindVO.needCountDown = true;
 //obj.data.saleRemindVO.countdownTime = 1832013888;
 
 body = JSON.stringify(obj);
