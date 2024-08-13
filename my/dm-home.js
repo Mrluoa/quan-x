aedc47a3a260ba3a06ab32c8b698694379470265
mrluoa <luoy@cdsimayi.com.cn>
2024-08-13 18:21:23 +0800

    fix:update

--- my/dm-home.js
+++ my/dm-home.js
@@ -7,38 +7,11 @@ var obj = JSON.parse(body);
 
 if (obj.data && obj.data.result) {
     var result = JSON.parse(obj.data.result);
-    result.itemBuyBtn = {
-        "btnStatus": "204",
-        "btnText": "立即购买",
-        "btnOrigin": "12"
+    result.detailViewComponentMap.item.item = {
+        "buyBtnStatus": "204",
+        "buyBtnText": "立即购买",
+        "buyBtnOrigin": "12",
     };
-    if (result.perform) {
-        //result.perform.saleForm = "1";
-
-        result.perform.skuList.forEach(item => {
-            item.frontEndStatus = "1";
-            item.mq = "6";
-            item.salableQuantity = "6";
-            item.skuSalable = "true";
-            item.tags = [];
-            item.mktPromotionTips = "";
-
-            delete item.followRelationTargetType;
-            delete item.otherTag;
-            delete item.tips;
-        })
-    }
-
-    if (result.performCalendar) {
-        result.performCalendar.performViews.forEach(item => {
-            item.buyPermission = true;
-            item.salable = true;
-            item.clickable = true;
-            item.tags = [];
-            delete item.otherTag;
-        })
-    }
-
     obj.data.result = JSON.stringify(result);
 }
 
