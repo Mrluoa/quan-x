6047db1d2bfd035f29b43a431b8e015d75c46534
mrluoa <luoy@cdsimayi.com.cn>
2024-09-06 14:12:23 +0800

    fix:update

--- my/dm-home.js
+++ my/dm-home.js
@@ -33,6 +33,33 @@ if (obj.data && obj.data.legacy) {
 
 }
 
+if (obj.data && obj.data.result) {
+    var result = JSON.parse(obj.data.result);
+    result.detailViewComponentMap.item.item.buyBtnStatus = "204";
+    result.detailViewComponentMap.item.item.buyBtnText = "立即购买";
+    result.detailViewComponentMap.item.item.buyBtnOrigin = "12";
+    result.detailViewComponentMap.item.item.sellStartTime = Date.now();
+    delete result.detailViewComponentMap.item.buyBtnTip;
+    delete result.detailViewComponentMap.item.statusNotice;
+    if (result.detailViewComponentMap.item.item.performBases) {
+        result.detailViewComponentMap.item.item.performBases.forEach(item => {
+            if (item.performs) {
+                item.performs.forEach(subitem => {
+                    subitem.saleForm = 1;
+                })
+            }
+
+        });
+    }
+
+    delete result.detailViewComponentMap.item.staticData.ticketPurchasesGuidePage;
+    result.detailViewComponentMap.item.staticData.itemBase.isHotProject = true;
+    result.detailViewComponentMap.item.staticData.itemBase.isShowGrabTicketGuide = false;
+    result.detailViewComponentMap.item.dynamicExtData.subFlag = false;
+
+    obj.data.result = JSON.stringify(result);
+}
+
 body = JSON.stringify(obj);
 
 $done({ body });
--- my/dm-info.js
+++ my/dm-info.js
@@ -8,9 +8,9 @@ var obj = JSON.parse(body);
 if (obj.data && obj.data.result) {
     var result = JSON.parse(obj.data.result);
 
-    // result.actionControl.renderingControl = {
-    //     "renderingType": "1"
-    // };
+    result.actionControl.renderingControl = {
+        "renderingType": "1"
+    };
 
     result.itemBuyBtn.btnStatus = "204";
     result.itemBuyBtn.btnText = "立即购买";
@@ -24,12 +24,13 @@ if (obj.data && obj.data.result) {
         result.perform.skuList.forEach(item => {
             item.frontEndStatus = "1";
             item.mq = "6";
-            item.salableQuantity = "4";
+            item.salableQuantity = result.perform.limitQuantity;
             item.skuSalable = "true";
             item.tags = [];
             item.status = "1";
             item.mktPromotionTips = "";
 
+            delete item.mktPromotionTips;
             delete item.followRelationTargetType;
             delete item.otherTag;
             delete item.tips;
