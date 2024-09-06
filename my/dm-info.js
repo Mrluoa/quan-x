9ac2321301e33ae695c529bf8fccf8d2dbfd9e53
mrluoa <luoy@cdsimayi.com.cn>
2024-09-06 14:30:57 +0800

    fix:update

--- my/dm-info.js
+++ my/dm-info.js
@@ -5,51 +5,51 @@
 var body = $response.body;
 var obj = JSON.parse(body);
 
-if (obj.data && obj.data.result) {
-    var result = JSON.parse(obj.data.result);
+// if (obj.data && obj.data.result) {
+//     var result = JSON.parse(obj.data.result);
 
-    result.actionControl.renderingControl = {
-        "renderingType": "1"
-    };
+//     result.actionControl.renderingControl = {
+//         "renderingType": "1"
+//     };
 
-    result.itemBuyBtn.btnStatus = "204";
-    result.itemBuyBtn.btnText = "立即购买";
-    result.itemBuyBtn.btnOrigin = "12";
+//     result.itemBuyBtn.btnStatus = "204";
+//     result.itemBuyBtn.btnText = "立即购买";
+//     result.itemBuyBtn.btnOrigin = "12";
 
-    if (result.perform) {
-        result.perform.saleForm = "1";
+//     if (result.perform) {
+//         result.perform.saleForm = "1";
 
-        result.perform.presale = "false";
+//         result.perform.presale = "false";
 
-        result.perform.skuList.forEach(item => {
-            item.frontEndStatus = "1";
-            item.mq = "6";
-            item.salableQuantity = result.perform.limitQuantity;
-            item.skuSalable = "true";
-            item.tags = [];
-            item.status = "1";
-            item.mktPromotionTips = "";
+//         result.perform.skuList.forEach(item => {
+//             item.frontEndStatus = "1";
+//             item.mq = "6";
+//             item.salableQuantity = result.perform.limitQuantity;
+//             item.skuSalable = "true";
+//             item.tags = [];
+//             item.status = "1";
+//             item.mktPromotionTips = "";
 
-            //delete item.mktPromotionTips;
-            delete item.followRelationTargetType;
-            delete item.otherTag;
-            delete item.tips;
-        })
-    }
+//             //delete item.mktPromotionTips;
+//             delete item.followRelationTargetType;
+//             delete item.otherTag;
+//             delete item.tips;
+//         })
+//     }
 
-    if (result.performCalendar) {
-        result.performCalendar.performViews.forEach(item => {
-            item.buyPermission = "true";
-            item.salable = "true";
-            item.clickable = "true";
-            item.checked = "true";
-            item.tags = [];
-            delete item.otherTag;
-        })
-    }
+//     if (result.performCalendar) {
+//         result.performCalendar.performViews.forEach(item => {
+//             item.buyPermission = "true";
+//             item.salable = "true";
+//             item.clickable = "true";
+//             item.checked = "true";
+//             item.tags = [];
+//             delete item.otherTag;
+//         })
+//     }
 
-    obj.data.result = JSON.stringify(result);
-}
+//     obj.data.result = JSON.stringify(result);
+// }
 
 body = JSON.stringify(obj);
 
